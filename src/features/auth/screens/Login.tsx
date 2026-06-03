98e92e324eff65d66a3fc95224bc4c721b9ee701
PandaBear-1 <oluwadamisi.samuel@gmail.com>
2026-06-04 00:43:58 +0100

    feat: made the login page responsive

--- src/features/auth/screens/Login.tsx
+++ src/features/auth/screens/Login.tsx
@@ -24,7 +24,7 @@ const LoginScreen = () => {
                 <h2 className='font-black text-2xl text-center'>Welcome Back</h2>
                 <p className='text-[#6E5F54] text-sm text-center my-2'>Sign in to access your dashboard</p>
 
-                <div className='bg-white w-105 p-7 rounded-[40px] mt-5'>
+                <div className='bg-white w-90 md:w-105 p-5 md:p-7 rounded-[40px] mt-5'>
                     <p className='text-[#6E5F54] font-bold text-xs'>Admin Email</p>
                     <input type="text" placeholder='admin@fashionhouse.com' className='border-1 border-[#E8E1D999] rounded-xl bg-[#F7F4F0] px-3 py-2.5 w-full mt-2 text-xs font-bold text-black placeholder:text-[rgba(42, 31, 26, 0.5)] placeholder:text-xs placeholder:font-bold focus:outline-none'  />
 
