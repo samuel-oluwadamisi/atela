06928c8c58ed6effc36f341cdad939a7dd1be02e
PandaBear-1 <oluwadamisi.samuel@gmail.com>
2026-06-12 21:36:46 +0100

    feat: added skeleton for danger zone route

---
+++ src/app/(dashboard)/admin/settings/danger-zone/page.tsx
@@ -0,0 +1,12 @@
+import DangerZoneScreen from '@/features/auth/screens/DangerZone'
+import React from 'react'
+
+const DangerZonePage = () => {
+  return (
+    <div>
+        <DangerZoneScreen />
+    </div>
+  )
+}
+
+export default DangerZonePage
