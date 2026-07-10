6fba7601fc6b26351ee3155132ecf539997d5c45
PandaBear-1 <oluwadamisi.samuel@gmail.com>
2026-07-10 22:27:23 +0100

    feat: added Ui changes to the staff info box

--- src/features/staff/dashboard/components/InfoCard.tsx
+++ src/features/staff/dashboard/components/InfoCard.tsx
@@ -8,8 +8,8 @@ type Props = {
 const InfoCard = ({info}: {info:Props}) => {
   return (
     <div className='w-full h-20 bg-white border border-[#E8E1D9] shadow-sm rounded-3xl px-4 py-4'>
-        <p>{info.name}</p>
-        <p>{info.num}</p>
+        <p className='font-bold text-[#C1785A]'>{info.name}</p>
+        <p className='text-[#2A1F1A]  font-bold text-lg'>{info.num}</p>
     </div>
   )
 }
