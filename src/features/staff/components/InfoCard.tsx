6e5405cc2fb45c66767da43cdca9c9d140de851f
PandaBear-1 <oluwadamisi.samuel@gmail.com>
2026-07-12 23:23:47 +0100

    feat: fixed responsive issues on the earnings route of the staff dashboard

--- src/features/staff/components/InfoCard.tsx
+++ src/features/staff/components/InfoCard.tsx
@@ -9,7 +9,7 @@ const InfoCard = ({info}: {info:Props}) => {
   return (
     <div className='flex flex-col flex-1 md:justify-between gap-5 md:gap-0 w-full h-22 md:h-30 bg-white border border-[#E8E1D9] shadow-sm rounded-xl md:rounded-3xl p-1 pl-2 md:p-4'>
         <p className='font-bold text-[#C1785A] text-base  '>{info.name}</p>
-        <p className='text-[#2A1F1A] font-bold text-2xl md:text-3xl '>{info?.money && '₦'}{info?.num?.toLocaleString('en-US')} </p>
+        <p className='text-[#2A1F1A] font-bold text-xl md:text-3xl '>{info?.money && '₦'}{info?.num?.toLocaleString('en-US')} </p>
     </div>
   )
 }
