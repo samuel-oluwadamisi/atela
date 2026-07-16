5dfffe8233029ef0d4b33c99c233d6245420b6ca
PandaBear-1 <oluwadamisi.samuel@gmail.com>
2026-07-16 23:26:48 +0100

    feat: fixed filter issues by accepting id prop as string since nextJs always sends id as string and never number

--- src/app/(dashboard)/staff/task-manager/[id]/page.tsx
+++ src/app/(dashboard)/staff/task-manager/[id]/page.tsx
@@ -4,13 +4,13 @@ import TaskManagerFullPageScreen from '@/features/staff/task-manager/screens/Tas
 
 type Props = {
   params: Promise<{
-    id: number;
+    id: string;
   }>;
 };
 const TaskManagerFullPage = async({params}: Props) => {
 
     const { id } = await params;
-    const task = getTaskById(id);
+    const task = getTaskById(Number(id));
   if (!task) 
     return <p className='text-red-600'>Task Not Found</p>;
 
