cf0e8da8ed8ad867f36411b9966e573d7cb0957c
samuel-oluwadamisi <oluwadamisi.samuel@gmail.com>
2026-08-07 23:35:35 +0200

    feat: edited the info box content

--- src/features/admin/dashboard/screens/DashboardHome.tsx
+++ src/features/admin/dashboard/screens/DashboardHome.tsx
@@ -7,10 +7,10 @@ import FloorStatus from '../components/FloorStatus'
 const DashboardHomeScreen = () => {
 
     const data = [
-        {name: 'ACTIVE ORDERS', num: 3, icon: '/images/atela-logo.png', extra: 'Action Required'},
-        {name: 'PENDING TASKS ', num: 12, icon: '/images/atela-logo.png', extra: 'Action Required'},
-        {name: 'LOW STOCK ITEMS', num: 1, icon: '/images/atela-logo.png', extra: 'Action Required'},
-        {name: 'LOW STOCK ITEMS', num: 1, icon: '/images/atela-logo.png', extra: 'Action Required'}
+        {name: 'ACTIVE ORDERS', num: 6, icon: '/images/atela-logo.png', extra: 'Order Directory'},
+        {name: 'PENDING TASKS ', num: 4, icon: '/images/atela-logo.png', extra: 'Team Output'},
+        {name: 'LOW STOCK ITEMS', num: 2, icon: '/images/atela-logo.png', extra: 'Inventory'},
+        {name: 'ORDERS DUE TODAY', num: 3, icon: '/images/atela-logo.png', extra: 'Action Required'}
     ]
   return (
     <main>
